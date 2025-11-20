ddbc2c947550b55ad7b87c408e466c6e6bd44563
Joe Rawnsley <joe.rawnsley@madetech.com>
2025-11-20 15:23:23 +0000

    recreate first page of portfolio site. colour mode button disabled.

--- portfolio_site_2/portfolio_site_2.py
+++ portfolio_site_2/portfolio_site_2.py
@@ -8,29 +8,58 @@ from rxconfig import config
 class State(rx.State):
     """The app state."""
 
+def navbar_link(text: str, url: str) -> rx.Component:
+    return rx.link(rx.text(text, size="4", weight="medium"), href=url)
 
 def index() -> rx.Component:
     # Welcome Page (Index)
     return rx.container(
-        rx.color_mode.button(position="top-right"),
+        # rx.color_mode.button(position="bottom-left"),
         rx.vstack(
-            rx.heading("Welcome to Reflex!", size="9"),
-            rx.text(
-                "Get started by editing ",
-                rx.code(f"{config.app_name}/{config.app_name}.py"),
-                size="5",
+            rx.heading("Joe Rawnsley - Home", size="8"),
+            rx.hstack(
+                rx.link("Home", href="https://reflex.dev/", flex="1"),
+                rx.link("About", href="https://reflex.dev/", flex="1"),
+                rx.link("Contact", href="https://reflex.dev/", flex="1"),
+                rx.link("Projects", href="https://reflex.dev/", flex="1"),
+                justify="center"
             ),
-            rx.link(
-                rx.button("Check out our docs!"),
-                href="https://reflex.dev/docs/getting-started/introduction/",
-                is_external=True,
-            ),
-            spacing="5",
-            justify="center",
-            min_height="85vh",
+        rx.text("I'm Joe Rawnsley, an apprentice software developer and devops engineer based in Manchester. Check out the section links above for more info."),
+        rx.image(src="/face.jpg")
         ),
     )
 
 
-app = rx.App()
+style = {
+    "background-color": "lightyellow",
+    rx.image: {
+        "height": "20rem",
+        "width": "auto",
+        "margin": "auto",
+        "border-radius": "5%"
+    },
+    rx.vstack: {
+        "margin": "auto"
+    },
+    rx.hstack: {
+        "width": "100%",
+        "margin": "auto",
+        "background-color": "lightgreen",
+    },
+    rx.heading: {
+      "text-align": "center"  
+    },
+    # rx.link: {
+    #    "background-color": "lightyellow", 
+    # }
+}
+
+
+
+
+
+app = rx.App(
+    style=style,
+)
+
 app.add_page(index)
