8310812b2326875e1a63a8bc6a7fe6ab3758962b
Joe Rawnsley <joe.rawnsley@madetech.com>
2025-11-20 17:03:54 +0000

    move some styles to separate CSS file, add about page and separate header component

--- portfolio_site_2/portfolio_site_2.py
+++ portfolio_site_2/portfolio_site_2.py
@@ -8,36 +8,42 @@ from rxconfig import config
 class State(rx.State):
     """The app state."""
 
-def navbar_link(text: str, url: str) -> rx.Component:
-    return rx.link(rx.text(text, size="4", weight="medium"), href=url)
+def header(page_title):
+    return rx.vstack(
+            title(page_title),
+            navbar(),
+            # rx.color_mode.button(position="bottom-left"),
+            )
+    
+def title(page_title):
+    return rx.heading(f"Joe Rawnsley - {page_title}", size="8"),
+    
+def navbar():
+    return rx.hstack(
+                rx.link("Home", href="/", flex="1"),
+                rx.link("About", href="/about", flex="1"),
+                rx.link("Contact", href="https://reflex.dev/", flex="1"),
+                rx.link("Projects", href="https://reflex.dev/", flex="1"),
+                justify="center"
+            )
 
 def index() -> rx.Component:
     # Welcome Page (Index)
     return rx.container(
-        # rx.color_mode.button(position="bottom-left"),
-        rx.vstack(
-            rx.heading("Joe Rawnsley - Home", size="8"),
-            rx.hstack(
-                rx.link("Home", href="https://reflex.dev/", flex="1"),
-                rx.link("About", href="https://reflex.dev/", flex="1"),
-                rx.link("Contact", href="https://reflex.dev/", flex="1"),
-                rx.link("Projects", href="https://reflex.dev/", flex="1"),
-                justify="center"
-            ),
+        header("Home"),
         rx.text("I'm Joe Rawnsley, an apprentice software developer and devops engineer based in Manchester. Check out the section links above for more info."),
         rx.image(src="/face.jpg")
         ),
-    )
 
+def about() -> rx.Component:
+    return rx.container(
+        header("About"),
+        rx.image(src="/face.jpg"),
+        rx.text("I'm Joe Rawnsley, an apprentice software developer and devops engineer based in Manchester. Check out the section links above for more info."),
+        rx.text("I'm Joe Rawnsley, an apprentice software developer and devops engineer based in Manchester. Check out the section links above for more info."),
+        )
 
 style = {
-    "background-color": "lightyellow",
-    rx.image: {
-        "height": "20rem",
-        "width": "auto",
-        "margin": "auto",
-        "border-radius": "5%"
-    },
     rx.vstack: {
         "margin": "auto"
     },
@@ -49,9 +55,9 @@ style = {
     rx.heading: {
       "text-align": "center"  
     },
-    # rx.link: {
-    #    "background-color": "lightyellow", 
-    # }
+    rx.link: {
+       "background-color": "lightgreen", 
+    }
 }
 
 
@@ -60,6 +66,10 @@ style = {
 
 app = rx.App(
     style=style,
+    stylesheets=[
+        "new_style.css"
+    ]
 )
 
 app.add_page(index)
+app.add_page(about)
